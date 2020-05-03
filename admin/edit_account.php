84f60a5ace3b27cd0dce3f1cf2071f0120f1b0db
ArmirHysesani <64379247+ArmirHysesani@users.noreply.github.com>
2020-05-03 20:18:39 +0200

    Update edit_account.php

--- admin/edit_account.php
+++ admin/edit_account.php
@@ -5,28 +5,38 @@
 ?>
 <html lang = "en">
 	<head>
-		<title>Hotel Online Reservation</title>
+		<title>Hotel PALAZZO</title>
 		<meta charset = "utf-8" />
 		<meta name = "viewport" content = "width=device-width, initial-scale=1.0" />
 		<link rel = "stylesheet" type = "text/css" href = "../css/bootstrap.css " />
 		<link rel = "stylesheet" type = "text/css" href = "../css/style.css" />
 	</head>
-<body>
-	<nav style = "background-color:rgba(0, 0, 0, 0.1);" class = "navbar navbar-default">
-		<div  class = "container-fluid">
-			<div class = "navbar-header">
-				<a class = "navbar-brand" >Hotel Online Reservation</a>
-			</div>
-			<ul class = "nav navbar-nav pull-right ">
-				<li class = "dropdown">
-					<a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class = "glyphicon glyphicon-user"></i> <?php echo $name;?></a>
-					<ul class="dropdown-menu">
-						<li><a href="logout.php"><i class = "glyphicon glyphicon-off"></i> Logout</a></li>
-					</ul>
-				</li>
-			</ul>
+	<br>
+	<br>
+	<br>
+	<br>
+    <body style = "background-image: url('images/bcg.png'); "></body>
+<header class="navbar navbar-inverse navbar-fixed-top bs-docs-nav" role="banner">
+  <div class="container">
+		<div class="navbar-header">
+		<a href="index2.html" class="navbar-brand">Hotel PALAZZO</a>
 		</div>
-	</nav>
+		<nav class="collapse-navbar" role="navigation">
+		<ul class="nav navbar-nav navbar-right">
+		<li><a href = "index2.html">Home</a></li> 
+		<li><a href = "aboutus.html">About Us</a></li> 
+		<li><a href = "gallery.html">Gallery</a></li>
+		<li><a href = "dineandlounge2.html">Dine and Lounge</a></li> 			
+		<li class="active"><a href = "reservation.php">Reservation</a></li> 
+		<li><a href = "rulesandregulation2.html">Rules and Regulation</a></li>
+        <li class="active">
+          <a href="contactus2.html">Contact</a>
+		</li>
+		<li style="margin-right: -80px; margin-left: 45px;border:0.5px  solid black;border-radius: 8px; font-size: 13px; font-weight: 600;"> <a href="admin/index.php"><img src="images/user-solid.png" />  Admin</a></li>
+      </ul>
+		</nav>
+  </div>
+</header>
 	<div class = "container-fluid">	
 		<ul class = "nav nav-pills">
 			<li><a href = "home.php">Home</a></li>
@@ -46,7 +56,7 @@
 				?>
 				<br />
 				<div class = "col-md-4">	
-					<form method = "POST" action = "edit_query_account.php?admin_id=<?php echo $fetch['admin_id']?>">
+				    <form method = "POST" action = "edit_query_account.php?admin_id=<?php echo $fetch['admin_id']?>">
 						<div class = "form-group">
 							<label>Name </label>
 							<input type = "text" class = "form-control" value = "<?php echo $fetch['name']?>" name = "name" />
@@ -70,10 +80,18 @@
 	</div>
 	<br />
 	<br />
-	<div style = "text-align:right; margin-right:10px;" class = "navbar navbar-default navbar-fixed-bottom">
-		<label>&copy; Copyright HOR 2017 </label>
-	</div>
+	
 </body>
+<br>
+	<br>
+	<br>
+	<br>
+<footer>
+	<div style = "text-align:right; " class = "footer">
+		<label style="color:white;  margin: 12px; margin-right: 20px; ">&copy; Copyright: DEFSA </label>
+	</div>
+	</footer>
+	
 <script src = "../js/jquery.js"></script>
 <script src = "../js/bootstrap.js"></script>
 </html>
