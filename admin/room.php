9ace1e9f9e201244c9740bde4a3d41659c8373a2
ArmirHysesani <64379247+ArmirHysesani@users.noreply.github.com>
2020-05-17 01:39:46 +0200

    Add files via upload

---
+++ admin/room.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<?php
+	require_once 'validate.php';
+	require 'name.php';
+?>
+<html lang = "en">
+	<head>
+		<title>Hotel PALAZZO</title>
+		<meta charset = "utf-8" />
+		<meta name = "viewport" content = "width=device-width, initial-scale=1.0" />
+		<link rel = "stylesheet" type = "text/css" href = "../css/bootstrap.css " />
+		<link rel = "stylesheet" type = "text/css" href = "../css/style.css" />
+	</head>
+<body style="background-color:#ffeaa7">
+	<header class="navbar navbar-inverse navbar-fixed-top bs-docs-nav" role="banner">
+  <div class="container">
+		<div class="navbar-header">
+		<a href="home.php" class="navbar-brand">Hotel PALAZZO</a>
+		</div>
+		<nav class="collapse-navbar" role="navigation">
+		<ul class="nav navbar-nav navbar-right">
+		<li  class = "dropdown">
+					<a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class = "glyphicon glyphicon-user"></i>  Administrator</a>
+					<ul class="dropdown-menu">
+						<li><a href="logout.php"><i class = "glyphicon glyphicon-off"></i> Logout</a></li>
+					</ul>
+				</li>
+      </ul>
+		</nav>
+  </div>
+</header>
+<br>
+<br>
+	<div class = "container-fluid">	
+		<ul class = "nav nav-pills">
+			<li><a href = "home.php">Home</a></li>
+			<li><a href = "account.php">Accounts</a></li>
+			<li><a href = "reserve.php">Reservation</a></li>
+			<li><a href = "room.php">Room</a></li>			
+		</ul>	
+	</div>
+	<br />
+	<div class = "container-fluid">
+		<div class = "panel panel-default">
+			<div class = "panel-body">
+				<div class = "alert alert-info">Transaction / Room</div>
+				<a class = "btn btn-success" href = "add_room.php"><i class = "glyphicon glyphicon-plus"></i> Add Room</a>
+				<br />
+				<br />
+				<table id = "table" class = "table table-bordered">
+					<thead>
+						<tr>
+							<th>Room Type</th>
+							<th>Price</th>
+							<th>Photo</th>
+							<th>Action</th>
+						</tr>
+					</thead>
+					<tbody>
+					<?php
+						$query = $conn->query("SELECT * FROM `room`") or die(mysqli_error());
+						while($fetch = $query->fetch_array()){
+					?>	
+						<tr>
+							<td><?php echo $fetch['room_type']?></td>
+							<td><?php echo $fetch['price']?></td>
+							<td><center><img src = "../photo/<?php echo $fetch['photo']?>" height = "50" width = "50"/></center></td>
+							<td><center><a class = "btn btn-warning" href = "edit_room.php?room_id=<?php echo $fetch['room_id']?>"><i class = "glyphicon glyphicon-edit"></i> Edit</a> <a class = "btn btn-danger" onclick = "confirmationDelete(this); return false;" href = "delete_room.php?room_id=<?php echo $fetch['room_id']?>"><i class = "glyphicon glyphicon-remove"></i> Delete</a></center></td>
+						</tr>
+					<?php
+						}
+					?>	
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+	<br />
+	<br />
+	<div style = "text-align:right;height: 40px; background-color: #003300;" class = "navbar navbar-default navbar-fixed-bottom">
+	<label style="color:white;  margin: 12px; margin-right: 20px; ">&copy; Copyright: DEFSA </label>
+	</div>
+</body>
+<script src = "../js/jquery.js"></script>
+<script src = "../js/bootstrap.js"></script>
+<script src = "../js/jquery.dataTables.js"></script>
+<script src = "../js/dataTables.bootstrap.js"></script>	
+<script type = "text/javascript">
+	function confirmationDelete(anchor){
+		var conf = confirm("Are you sure you want to delete this record?");
+		if(conf){
+			window.location = anchor.attr("href");
+		}
+	} 
+</script>
+
+<script type = "text/javascript">
+	$(document).ready(function(){
+		$("#table").DataTable();
+	});
+</script>
+</html>
