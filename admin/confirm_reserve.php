d454ce12dc7fda200090360d94b8afe7cdc5ec6c
aishetopi <51795166+aishetopi@users.noreply.github.com>
2020-06-01 23:28:31 +0200

    Add files via upload

---
+++ admin/confirm_reserve.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<?php
+	require_once 'validate.php';
+	require 'name.php';
+?>
+<html lang = "en">
+	<head>
+		<title>Hotel PALAZZO</title>
+		<meta charset = "utf-8" />
+		<meta name = "viewport" content = "width=device-width, initial-scale=1.0" />
+		<link rel = "stylesheet" type = "text/css" href = "../css/bootstrap.css " />
+		<link rel = "stylesheet" type = "text/css" href = "../css/style.css" />
+	</head>
+<body style="background-color:#ffeaa7">
+<header class="navbar navbar-inverse navbar-fixed-top bs-docs-nav" role="banner">
+  <div class="container">
+		<div class="navbar-header">
+		<a href="home.php" class="navbar-brand">Hotel PALAZZO</a>
+		</div>
+		<nav class="collapse-navbar" role="navigation">
+		<ul class="nav navbar-nav navbar-right">
+		<li  class = "dropdown">
+					<a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class = "glyphicon glyphicon-user"></i>  Administrator</a>
+					<ul class="dropdown-menu">
+						<li><a href="logout.php"><i class = "glyphicon glyphicon-off"></i> Logout</a></li>
+					</ul>
+				</li>
+      </ul>
+		</nav>
+  </div>
+</header>
+<br>
+
+	<div class = "container-fluid">
+		<div class = "panel panel-default">
+			<div class = "panel-body">
+				<div class = "alert alert-info">Fill up form</div>
+				<?php
+					$query = $conn->query("SELECT * FROM `transaction` NATURAL JOIN `guest` NATURAL JOIN `room` WHERE `transaction_id` = '$_REQUEST[transaction_id]'") or die(mysqli_error());
+					$fetch = $query->fetch_array();
+				?>
+				<br />
+				<form method = "POST" enctype = "multipart/form-data" action = "save_form.php?transaction_id=<?php echo $fetch['transaction_id']?>">
+					<div class = "form-inline" style = "float:left;">
+						<label>Firstname</label>
+						<br />
+						<input type = "text" value = "<?php echo $fetch['firstname']?>" class = "form-control" size = "40" disabled = "disabled"/>
+					</div>
+					<div class = "form-inline" style = "float:left; margin-left:20px;">
+						<label>Middlename</label>
+						<br />
+						<input type = "text" value = "<?php echo $fetch['middlename']?>" class = "form-control" size = "40" disabled = "disabled"/>
+					</div>
+					<div class = "form-inline" style = "float:left; margin-left:20px;">
+						<label>Lastname</label>
+						<br />
+						<input type = "text" value = "<?php echo $fetch['lastname']?>" class = "form-control" size = "40" disabled = "disabled"/>
+					</div>
+					<br style = "clear:both;"/>
+					<br />
+					<div class = "form-inline" style = "float:left;">
+						<label>Room Type</label>
+						<br />
+						<input type = "text" value = "<?php echo $fetch['room_type']?>" class = "form-control" size = "20" disabled = "disabled"/>
+					</div>
+					<div class = "form-inline" style = "float:left; margin-left:20px;">
+						<label>Room No</label>
+						<br />
+						<input type = "number" min = "0" max = "999" name = "room_no" class = "form-control" required = "required"/>
+					</div>
+					<div class = "form-inline" style = "float:left; margin-left:20px;">
+						<label>Days</label>
+						<br />
+						<input type = "number" min = "0" max = "99" name = "days" class = "form-control" required = "required"/>
+					</div>
+					<div class = "form-inline" style = "float:left; margin-left:20px;">
+						<label>Extra Bed</label>
+						<br />
+						<input type = "number" min = "0" max = "99" name = "extra_bed" class = "form-control"/>
+					</div>
+					<div class = "form-inline" style = "float:left; margin-left:20px;">
+						<label style = "color:#ff0000;">*Extra Bed cost 800</label>
+					</div>
+					<br style = "clear:both;"/>
+					<br />
+					<button name = "add_form" class = "btn btn-primary"><i class = "glyphicon glyphicon-save"></i> Submit</button>
+				</form>
+			</div>
+		</div>
+	</div>
+	<br />
+	<br />
+	<div style = "text-align:right;height: 40px; background-color: #003300;" class = "navbar navbar-default navbar-fixed-bottom">
+	<label style="color:white;  margin: 12px; margin-right: 20px; ">&copy; Copyright: DEFSA </label>
+	</div>
+</body>
+<script src = "../js/jquery.js"></script>
+<script src = "../js/bootstrap.js"></script>	
+</html>
