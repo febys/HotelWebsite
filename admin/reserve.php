1656502ef9f2bd68ba6f5b275b5f130417716189
ArmirHysesani <64379247+ArmirHysesani@users.noreply.github.com>
2020-06-02 01:00:24 +0200

    Update reserve.php

--- admin/reserve.php
+++ admin/reserve.php
@@ -35,8 +35,8 @@
 	<div class = "container-fluid">	
 		<ul class = "nav nav-pills">
 			<li><a href = "home.php">Home</a></li>
-			<li class = "active"><a href = "account.php">Accounts</a></li>
-			<li><a href = "reserve.php">Reservation</a></li>
+			<li><a href = "account.php">Accounts</a></li>
+			<li class = "active"><a href = "reserve.php">Reservation</a></li>
 			<li><a href = "room.php">Room</a></li>			
 		</ul>	
 	</div>
